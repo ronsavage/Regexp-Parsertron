e4af7dce51c3c83546264a237eb5bf0faed91508
ronsavage <ron@savage.net.au>
2018-02-12 13:25:07 +1100

    Update version # to 1.02

--- lib/Regexp/Parsertron/Actions.pm
+++ lib/Regexp/Parsertron/Actions.pm
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 #use warnings qw(FATAL utf8); # Fatalize encoding glitches.
 
-our $VERSION = '1.01';
+our $VERSION = '1.02';
 
 # ------------------------------------------------
 
